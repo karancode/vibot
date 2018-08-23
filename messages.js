e25372134352ee223a157091826a95d72e0fda4f
kdthanvi <thanvikaran95@gmail.com>
2018-08-23 11:23:29 +0900

    add messages

---
+++ messages.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    ATTENDANCE_STATUS : "Hello! Please share your attendance status for today! \n\
+    You can choose from the options displayed to your keyboard :) ",
+
+    LATE_TIME : "Oh! Please tell me for how long you are going to be late ? ",
+
+    DAYOFF_REASON : "Oh! Please tell me the reason for youe Day Off! ",
+
+    HALFDAYOFF_REASON : "Oh! Please tell me whether you will take AM Off or PM Off ! ",
+
+    RECORD_REASON : "Understood! Could you please tell the reason ? "
+
+};
