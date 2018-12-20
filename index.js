d1944d12f5835e11811df5c70432314955f1640a
kdthanvi <thanvikaran95@gmail.com>
2018-12-20 16:12:02 +0900

    add email content message for each case

--- index.js
+++ index.js
@@ -58,6 +58,24 @@ function reason_keyboard(response){
     //response.send(new KeyboardMessage(keyboards.REASON_KEYBOARD));
 }
 
+// function for sending email
+function send_email(response){
+    let mail_options = {
+        from : mail_config.SENDER_EMAIL,
+        to : mail_config.RECEIVER_EMAIL,
+        subject : `[Attendance]${response.userProfile.name}`,
+        text : `${attendance_text}`
+    };
+    email.mailtransporter.sendMail(mail_options, function(error, info){
+        if(error){
+            console.log("error happened: " + error);
+        }else{
+            console.log("Email sent: " + info.response);
+            response.send(new TextMessage("Noted. Thanks!"));
+        }
+    });
+}
+
 // Creating the bot with access token, name and avatar
 const bot = new ViberBot(logger, {
     authToken: localConfig.viber_auth_token, // <--- Paste your token here
@@ -69,57 +87,63 @@ bot.onSubscribe(response => {
     say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you.\
      Just send me a name of a website and I'll do the rest!`);
 });
-let attendance_text = "This is the attendance text...";
+let attendance_text = "\n=====================================\n Attendance Details/出席の詳細 \n=====================================\n";
+let date = new Date();
 
 bot.onTextMessage(/./, (message, response) => {
     switch(message.text) {
         case 'Hi':
             on_option_kyboard(response);
-            attendance_text = "\n =============================\n Attendance Details \n =============================\n";
+            attendance_text = `\n Date/日付 : ${date}`;
             break;
         case 'Late':
             on_late_keyboard(response); 
-            attendance_text = attendance_text + "\n Status : Late";
+            attendance_text = attendance_text + "\n Arrival Status/到着ステータス : Late Arrival/遅れた到着";
             break;
         case '5to10min':
+            reason_keyboard(response);
+            attendance_text = attendance_text + "\n Late by/後で : 5-10 minutes/5〜10分";
+            break;
         case '30min':
+            reason_keyboard(response);
+            attendance_text = attendance_text + "\n Late by/後で : 30 minutes/30分";
+            break;
         case '1hr':
+            reason_keyboard(response);
+             attendance_text = attendance_text + "\n Late by/後で : 1 hour /1時間";
+             break;
         case 'more1hr':
             reason_keyboard(response);
-            attendance_text = attendance_text + "\n Late by : <<time>>";
+            attendance_text = attendance_text + "\n Late by/後で : More than 1 hour/1時間以上";
             break;    
         case 'DayOff':
             on_dayoff_keyboard(response);
-            attendance_text = attendance_text + "\n Status : Day Off";
+            attendance_text = attendance_text + "\n Arrival Status/到着ステータス : Day Off/休みの日";
             break;
         case 'HalfDayOff':
             on_halfdayoff_keyboard(response);
-            attendance_text = attendance_text + "\n Status : Half Day Off";
+            attendance_text = attendance_text + "\n Arrival Status : Half Day Off/半日休み";
             break;
         case 'amoff':
+            reason_keyboard(response);
+            attendance_text = attendance_text + "\n Off Period/オフ期間 : AM Off/AMオフ";
+            break;
         case 'pmoff':
             reason_keyboard(response);
-            attendance_text = attendance_text + "\n Period : <<am/pm>>";
+            attendance_text = attendance_text + "\n Off Period/オフ期間 : PM Off/PMオフ";
             break;
         case 'privatereason':
+            attendance_text = attendance_text + "\n Reason/理由 : Private Reason/私的理由";
+            send_email(response);
         case 'traindelay':
+            attendance_text = attendance_text + "\n Reason/理由 : Train Delay/列車遅延";
+            send_email(response);
         case 'badhealth':
+            attendance_text = attendance_text + "\n Reason/理由 : Bad Health/健康状態が悪い";
+            send_email(response);
         case 'trainingtrip':
-            attendance_text = attendance_text + "\n Reason : <<reason>>";
-            let mail_options = {
-                from : mail_config.SENDER_EMAIL,
-                to : mail_config.RECEIVER_EMAIL,
-                subject : `[Attendance]${response.userProfile.name}`,
-                text : `${attendance_text}`
-            };
-            email.mailtransporter.sendMail(mail_options, function(error, info){
-                if(error){
-                    console.log("error happened: " + error);
-                }else{
-                    console.log("Email sent: " + info.response);
-                    response.send(new TextMessage("Noted. Thanks!"));
-                }
-            });
+            attendance_text = attendance_text + "\n Reason/理由 : Office Training/オフィストレーニング";
+            send_email(response);
             break;
         default :
             response.send(new TextMessage("Sorry I do not understand. Please send \"Hi\" "));
